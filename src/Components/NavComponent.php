206be46b9c5be61649475534430d2e6c5a74b440
damodev <damodev@protonmail.com>
2023-04-15 21:58:19 +0200

    Create API + play

---
+++ src/Components/NavComponent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MyWebsite\Components;
+
+use Dupot\StaticGenerationFramework\Component\ComponentAbstract;
+use Dupot\StaticGenerationFramework\Component\ComponentInterface;
+use MyWebsite\Pages\HomePage;
+
+class NavComponent extends ComponentAbstract implements ComponentInterface
+{
+
+    protected $pageSelected;
+
+    public function __construct($pageSelected)
+    {
+        $this->pageSelected = $pageSelected;
+    }
+
+    public function render(): string
+    {
+        $linkList = [
+            'Home' => HomePage::FILENAME,
+
+        ];
+
+        return $this->renderViewWithParamList(
+            __DIR__ . '/Nav/nav.php',
+            [
+                'linkList' => $linkList,
+                'pageSelected' => $this->pageSelected
+            ]
+        );
+    }
+}
